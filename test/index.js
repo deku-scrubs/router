1516817f0ad9a54f2a90756b92877935de36d420
Andrew Shaffer <darawk@gmail.com>
2015-08-18 01:19:26 -0700

    add test for subroute wildcard matching

--- test/index.js
+++ test/index.js
@@ -11,7 +11,7 @@ import router from '..'
  * Tests
  */
 
-test('should work', function ({equal, fail, plan}) {
+test('should work', ({equal, fail, plan}) => {
   const App = router({
     '/user/:user': {
       render ({props}) { equal(props.params.user, 'test') }
@@ -23,6 +23,20 @@ test('should work', function ({equal, fail, plan}) {
 
   plan(1)
   create(<App url='/user/test' />)
+})
+
+test('should pass the rest of the url to a wildcard route', ({equal, plan}) => {
+  const App = router({
+    '/user/:user/*': {
+      render({props}) {
+        equal(props.params.user, 'test')
+        equal(props.rest, '/subroute/subroute2')
+      }
+    }
+  })
+
+  plan(2)
+  create(<App url='/user/test/subroute/subroute2' />)
 })
 
 /**
