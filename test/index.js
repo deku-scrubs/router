79aa960662ae82e5a57e7d2ce81bd4a277741c18
Andrew Shaffer <darawk@gmail.com>
2015-08-23 18:31:40 -0700

    remove deku dependence

--- test/index.js
+++ test/index.js
@@ -3,8 +3,6 @@
  */
 
 import test from 'tape-catch'
-import {tree, renderString} from 'deku'
-import element from 'virtual-element'
 import router from '..'
 
 /**
@@ -13,56 +11,34 @@ import router from '..'
 
 test('should work', ({equal, fail, plan}) => {
   const App = router({
-    '/user/:user': () => ({
-      render ({props}) { equal(props.params.user, 'test') }
-    }),
-    '/otherThing': () => ({
-      render () { fail() }
-    })
+    '/user/:user': (url, params) => equal(params.user, 'test'),
+    '/otherThing': () => fail
   })
 
   plan(1)
-  create(<App url='/user/test' />)
+  App('/user/test')
 })
 
 test('should pass the rest of the url to a subroute', ({equal, plan}) => {
   const App = router({
-    '/user/:user': () => ({
-      render({props}) {
-        equal(props.params.user, 'test')
-        equal(props.url, '/subroute/subroute2')
-      }
-    })
+    '/user/:user': (url, params) => {
+      equal(params.user, 'test')
+      equal(url, '/subroute/subroute2')
+    }
   })
 
   plan(2)
-  create(<App url='/user/test/subroute/subroute2' />)
+  App('/user/test/subroute/subroute2')
 })
 
 test('should nest routes', ({pass, fail, plan}) => {
   const App = router({
-    '/user/:user': () => (router({
-      '/profile': () => ({
-        render () {
-          pass()
-        }
-      }),
-      '/feed': () => ({
-        render () {
-          fail()
-        }
-      })
-    }))
+    '/user/:user': router({
+      '/profile': () => pass(),
+      '/feed': () => fail()
+    })
   })
 
   plan(1)
-  create(<App url='/user/test/profile' />)
+  App('/user/test/profile')
 })
-
-/**
- * Helpers
- */
-
-function create (component) {
-  renderString(tree(component))
-}
